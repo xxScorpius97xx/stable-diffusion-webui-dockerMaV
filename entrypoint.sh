b4bea88593860d4f22924e06c8a49558f5cedcce
MaV <132741218+xxScorpius97xx@users.noreply.github.com>
2024-06-08 13:07:27 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Navigate to the web UI directory
-cd /stable-diffusion-webui
+exec /stable-diffusion-webui/webui.sh
 
-# Start the web UI with the defined command-line arguments
-python launch.py --listen --port=9999 --enable-insecure-extension-access --api
