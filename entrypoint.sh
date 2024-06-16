c2a34c551e28816580270c19edcbd3f4db180d03
MaV <132741218+xxScorpius97xx@users.noreply.github.com>
2024-06-16 08:51:30 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
-
+ #!/bin/bash
+wget https://huggingface.co/swl-models/chilloutmix-ni/resolve/main/chilloutmix-Ni.safetensors -P /stable-diffusion-webui/models/Stable-diffusion/ &&
+##wget https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.safetensors -P /stable-diffusion-webui/models/Stable-diffusion/ &&
 exec /stable-diffusion-webui/webui.sh
-
