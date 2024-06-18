282a4ebd12a1670a4b951a158b51defee1da561f
MaV <132741218+xxScorpius97xx@users.noreply.github.com>
2024-06-18 16:52:10 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download the file with a new filename and save it to the specified directory
-wget https://civitai.com/api/download/models/395827 -O /stable-diffusion-webui/models/Stable-diffusion/new_filename.safetensors &&
+wget https://civitai.com/api/download/models/395827 -O /stable-diffusion-webui/models/Stable-diffusion/mav_JibMixRealisticXL.safetensors &&
 
 # Execute the web UI script
 exec /stable-diffusion-webui/webui.sh
