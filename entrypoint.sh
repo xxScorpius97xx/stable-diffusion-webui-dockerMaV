0dece1cbc3c9abf0dbd6b5a7f234874484701794
MaV <132741218+xxScorpius97xx@users.noreply.github.com>
2024-06-22 19:06:37 +0700

    Update entrypoint.sh
    Checkpoint to Realism Engine SDXL 3.0

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download the file with a new filename and save it to the specified directory
-wget https://civitai.com/api/download/models/395827 -O /stable-diffusion-webui/models/Stable-diffusion/mav_JibMixRealisticLightning.safetensors &&
+wget https://civitai.com/api/download/models/293240 -O /stable-diffusion-webui/models/Stable-diffusion/mav_RealismEngineSDXL.safetensors &&
 #Jib Mix Realistic XL V10 Lightning 4-6 Step
 # Execute the web UI script
 exec /stable-diffusion-webui/webui.sh
